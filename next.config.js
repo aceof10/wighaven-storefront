a51c6f0bdfb97213f038ec17e0f47844b3e13b56
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2025-07-14 04:35:59 +0300

    chore: add S3 file url to next.config.js image urls

--- next.config.js
+++ next.config.js
@@ -36,6 +36,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "wighaven-files.s3.af-south-1.amazonaws.com",
+      },
     ],
   },
 }
